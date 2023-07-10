83746c66178c84e7d9b1190b183b5e1a9ec86504
idhanu <idhanu@gmail.com>
2023-07-11 01:35:02 +1000

    Updated to keep going when errors

--- src/test.py
+++ src/test.py
@@ -1,8 +1,11 @@
+import logging
+from time import sleep
 from dotenv import load_dotenv
 load_dotenv()
 
 import asyncio
 import os
+import sys
 from mail import send_email
 from tracker import Tracker
 
@@ -33,17 +36,18 @@ async def main():
     await manager.async_device_discovery()
     plugs = manager.find_devices(device_name="Synology")
 
-    if len(plugs) < 1:
-        print("No electricity-capable device found...")
-    else:
-        plug = plugs[0]
+    try:
+        if len(plugs) < 1:
+            raise Exception("No electricity-capable device found...")
+        else:
+            plug = plugs[0]
 
-        # Update device status: this is needed only the very first time we play with this device (or if the
-        # connection goes down)
-        await plug.async_update()
+            # Update device status: this is needed only the very first time we play with this device (or if the
+            # connection goes down)
+            await plug.async_update()
 
-        delay = STOPPED_DELAY
-        try:
+            delay = STOPPED_DELAY
+        
             while(True):
                 # Read the electricity power/voltage/current
                 instant_consumption = await plug.async_get_instant_metrics()
@@ -60,15 +64,21 @@ async def main():
                     delay = STOPPED_DELAY
                     record.clear()
                 
+                sys.stdout.flush()
                 await asyncio.sleep(delay)
-                
-        finally:
-            # Close the manager and logout from http_api
-            print("\n\nClosing connection. Please wait...")
-            manager.close()
-            await http_api_client.async_logout()
+    finally:
+        # Close the manager and logout from http_api
+        print("\n\nClosing connection. Please wait...")
+        manager.close()
+        await http_api_client.async_logout()
 
-try:
-    asyncio.run(main())
-except(KeyboardInterrupt):
-    print("Completed")
+while (True): 
+    try:
+        asyncio.run(main())
+    except(KeyboardInterrupt):
+        print("Completed")
+        break
+    except:
+        logging.exception('')
+        print("Above exception occurred - waiting 60 seconds and continuing")
+        sleep(60)
